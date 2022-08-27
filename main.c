1fdde376f26b89162bc404798c411c508131d42b
WorldOfBerk <baris.berk.sengl@hotmail.com>
2022-08-27 17:37:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <conio.h>
 #include <windows.h>
 
+#define ENTER 13
+#define TAB 9
+#define BACKSPC 8
+
 struct member{
     char fullName[100];
     char email[100];
@@ -28,9 +32,38 @@ void createUsername(char email[100], char username[50])
     }
 }
 
+void takePassword(char pass[50])
+{
+    int i = 0;
+    char ch;
+    while(1)
+    {
+        ch = getch();
+        if(ch == ENTER || ch == TAB)
+        {
+            pass[i] = '\0';
+            break;
+        }
+        else if(ch == BACKSPC)
+        {
+            if(i > 0)
+            {
+                i--;
+                printf("\b \b");
+            }
+        }
+        else
+        {
+            pass[i++] = ch;
+            printf("* \b");
+        }
+    }
+}
+
 int main()
 {
     int choice;
+    struct member member;
 
     printf("\n\t\t\t\t---------------Welcome to Log-in System---------------");
     printf("\nSelect one of the options>");
@@ -41,6 +74,19 @@ int main()
     printf("\n\nEnter your selection > ");
     scanf("%d", &choice);
     fgetc(stdin);
+
+    switch (choice)
+    {
+        case 1:
+            printf("\nEnter your full name: ");
+            takeinput(member.fullName);
+            printf("Enter your email: ");
+            takeinput(member.email);
+            printf("Enter your phone number: ");
+            takeinput(member.phoneNumber);
+            printf("Enter your password: ");
+            takePassword(member.password);
+     }
 
     return 0;
 }
